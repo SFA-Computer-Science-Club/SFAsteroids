7c3cb26f8aee9a4eb84b4240513a1d38ed2f97bf
MBraydenThacker <thackermb@jacks.sfasu.edu>
2024-02-23 23:45:01 -0600

    Added Title to MainMenu

--- Scenes/MainMenu.cs
+++ Scenes/MainMenu.cs
@@ -1,16 +1,19 @@
 using Godot;
 using System;
+using System.Reflection;
 
 public partial class MainMenu : Node2D
 {
 	private PackedScene gameScene;
 	private Node2D gameInstance;
 	private Button button;
+	private Node2D title;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		gameScene = GD.Load<PackedScene>("res://Scenes/Game.tscn");
 		button = GetNode<Button>("Button");
+		title = GetNode<Sprite2D>("Title");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,6 +26,7 @@ public partial class MainMenu : Node2D
 		gameInstance = (Node2D)gameScene.Instantiate();
 		AddChild(gameInstance);
 		button.Hide();
+		title.Hide();
 	}
 
 }
